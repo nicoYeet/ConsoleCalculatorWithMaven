b7f7d3cfddec8e625c4366d286f28cff0b00cbba
nico.leemann <nico.leemann@gibwintra.ch>
2020-01-17 14:14:07 +0100

    testee doesnt get created in the tests anymore

--- src/test/java/ch/bbw/consolecalculator/CalculatorTest.java
+++ src/test/java/ch/bbw/consolecalculator/CalculatorTest.java
@@ -15,25 +15,21 @@ public class CalculatorTest {
 
 	@Test
 	public void testSummeZweiPositiveIsOk() {
-		testee = new Calculator();
 		assertTrue(testee.summe(10, 25) == 35);
 	}
 	
 	@Test
 	public void testSubtractionZweiPositiveIsOk() {
-		testee = new Calculator();
 		assertTrue(testee.subtraktion(25, 10) == 15);
 	}
 	
 	@Test
 	public void testSummeEinPositiverEinNegativerIsOk() {
-		testee = new Calculator();
 		assertTrue(testee.summe(10, -5) == 5);
 	}
 	
 	@Test
 	public void testSubtractionEinPositiverEinNegativerIsOk() {
-		testee = new Calculator();
 		assertTrue(testee.subtraktion(10, -5) == 15);
 	}
 }
